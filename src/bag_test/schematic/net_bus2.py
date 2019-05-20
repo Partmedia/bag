162ff544545480d0a5dfbaa30842e6d502a877b6
Kourosh Hakhamaneshi <kourosh@login1.bcanalog.com>
2019-05-20 16:50:03 -0700

    added unit tests for schematic CDL generation which fails

--- src/bag_test/schematic/net_bus2.py
+++ src/bag_test/schematic/net_bus2.py
@@ -11,15 +11,15 @@ from bag.util.immutable import Param
 
 
 # noinspection PyPep8Naming
-class bag_test__net_bus(Module):
-    """Module for library bag_test cell net_bus.
+class bag_test__net_bus2(Module):
+    """Module for library bag_test cell net_bus2.
 
     Fill in high level description here.
     """
 
     yaml_file = pkg_resources.resource_filename(__name__,
                                                 os.path.join('netlist_info',
-                                                             'net_bus.yaml'))
+                                                             'net_bus2.yaml'))
 
     def __init__(self, database: ModuleDB, params: Param, **kwargs: Any) -> None:
         Module.__init__(self, self.yaml_file, database, params, **kwargs)
@@ -34,17 +34,18 @@ class bag_test__net_bus(Module):
             dictionary from parameter names to descriptions.
         """
         return dict(
-            mult='number of bits in the intermediate bus'
+            mult='number of bits'
         )
 
     def design(self, mult: int) -> None:
+        self.instances['X0'].design(mult=mult)
 
-        self.instances['X0'].design(mult=1)
-        self.instances['X1'].design(mult=mult)
+        bus_name = f'<{mult - 1}:0>'
+        in_name = f'vin{bus_name}'
+        out_name = f'vout{bus_name}'
 
-        if mult > 1:
-            bus_name = f'<{mult - 1}:0>'
-            mid_name = f'mid{bus_name}'
+        self.rename_pin('vin', in_name)
+        self.rename_pin('vout', f'vout{bus_name}')
+        self.reconnect_instance_terminal('X0', f'vin{bus_name}', in_name)
+        self.reconnect_instance_terminal('X0', f'vout{bus_name}', out_name)
 
-            self.rename_instance('X0', f'X0{bus_name}', [('vout', mid_name)])
-            self.reconnect_instance_terminal('X1', f'vin{bus_name}', mid_name)
--- src/bag_test/schematic/pin_array_0.py
+++ src/bag_test/schematic/pin_array_0.py
@@ -1,16 +1,4 @@
-# Copyright 2019 Blue Cheetah Analog Design Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# -*- coding: utf-8 -*-
 
 from typing import Dict, Any
 
@@ -53,5 +41,8 @@ class bag_test__pin_array_0(Module):
         if mult > 1:
             bus_name = f'<{mult - 1}:0>'
             in_name = f'vin{bus_name}'
+            out_name = f'vout{bus_name}'
             self.rename_pin('vin', in_name)
+            self.rename_pin('vout', out_name)
             self.rename_instance('XIN', f'XIN{bus_name}', [('noConn', in_name)])
+            self.rename_instance('XOUT', f'XOUT{bus_name}', [('noConn', out_name)])
